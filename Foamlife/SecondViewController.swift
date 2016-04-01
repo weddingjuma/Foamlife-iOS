99c163fb4347396fed97739a9143745eb2098b42
Kelvin <kgraddick@wavelinkllc.com>
2016-03-31 21:23:32 -0400

    Initial Commit

---
+++ Foamlife/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Foamlife
+//
+//  Created by Kelvin Graddick on 3/31/16.
+//  Copyright © 2016 Wave Link, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
